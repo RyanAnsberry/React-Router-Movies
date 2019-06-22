bc2a893d83972dffe3df053d9b43a811e129d4c5
Ryan Ansberry <ansman92@gmail.com>
2019-06-21 17:58:03 -0700

    Implemented saved movie list functionallity

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,11 +12,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <span key={movie.title} className="saved-movie">{movie.title}</span>
         ))}
-        <Link to="/">
+        <NavLink to="/" activeClassName="activeNavButton">
           <div className="home-button">Home</div>
-        </Link>
+        </NavLink>
       </div>
     );
   }
